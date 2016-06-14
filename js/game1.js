6d6f5ea6791ec436398e7d4552ffaf5809587313
Igor <mr.igfe@gmail.com>
2016-06-14 14:55:43 +0300

    change

---
+++ js/game1.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gQuests = [
+
+    {   id:1,
+        img:'<img id="1" src="img/layout/ca.png" />',
+        opts:['abc','def'],
+        correctOptIndex:0
+    },
+    {   id:2,
+        img:'<img id="2" src="img/layout/ca-white.png" />',
+        opts:['bca','edf'],
+        correctOptIndex:1
+    },
+    {   id:3,
+        img:'<img id="3" src="img/layout/logo.png" />',
+        opts:['cba','fed'],
+        correctOptIndex:0
+    }
+];
+
+console.log(gQuests);
+console.log(gQuests[0].opts[0]);
+
+
+$(document).ready(function(){
+$('.but1').html(gQuests[0].opts[0]);
+$('.but2').html(gQuests[0].opts[1]);
+$('.picture').html(gQuests[0].img);
+});
+
+function answer(index){
+var pictureID = $('.picture img').attr("id");
+console.log(pictureID);
+gQuests.forEach(function(gQuest){
+    if(pictureID===gQuest.)
+})
+};
