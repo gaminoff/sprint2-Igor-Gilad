48a92baf7f8b50d2614c339b33b9c21a672e95c0
Igor <mr.igfe@gmail.com>
2016-06-15 12:29:20 +0300

    s

--- js/game2.js
+++ js/game2.js
@@ -1,1 +1,54 @@
 
+'use strict';
+var KNIGHT=1;
+
+
+
+
+$(document).ready(function init(){
+    alert('hi');
+
+    var rows = [0,1,2,3,4,5,6,7,8,9,10].map(function(i){
+
+        var currRowHtml = '<tr>';
+
+        var cells = [0,1,2,3,4,5,6,7,8,9,10].map(function(j){
+            var cellColor = ((i + j) % 2 === 0)? 'dark' : 'bright'; 
+            var pawn = getPawn(i, j);
+
+            return '<td onclick="cellClicked(this)" ' + 
+                    ' data-row="'+i+'" data-col="' + j + '" ' +  
+                    ' class="'+cellColor+'">' + pawn  + '</td>';
+        });
+        currRowHtml += cells.join('');
+
+        currRowHtml += '</tr>';
+        return currRowHtml;
+    });
+
+
+    // console.log(rows);
+    $('.board').html(rows.join(''))
+
+
+});
+
+function getPawn(i, j) {
+    if (i === 4 && j === 4) return KNIGHT;
+    return '';
+}
+
+
+// function cellClicked(cell) {
+//     console.log('cell: ', cell);
+    
+
+//         var $cell = $(cell);
+//         var data = $cell.data();
+//     if ($cell.html() === KNIGHT) {
+     
+//     } 
+// } 
+// function checkPosibolMovment(date, innerHtml){
+    
+// }
