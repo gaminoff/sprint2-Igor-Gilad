92f52cefa1453a2387c13a4a0d337c1dd439de17
Igor <mr.igfe@gmail.com>
2016-06-16 13:49:06 +0300

    changes

--- js/game2.js
+++ js/game2.js
@@ -10,11 +10,11 @@ $(document).ready(function init(){
 
         var cells = [0,1,2,3,4,5,6,7,8,9].map(function(j){
             var cellColor = (j % 2 === 0)? 'dark' : 'bright'; 
-            var pawn = getPawn(i, j);
+            var num = getNum(i, j);
 
             return '<td onclick="cellClicked(this)" ' + 
                     ' data-row="'+i+'" data-col="' + j + '" ' +  
-                    ' class="'+cellColor+'">' + pawn  + '</td>';
+                    ' class="'+cellColor+' cell'+j+'">' + num  + '</td>';
         });
         currRowHtml += cells.join('');
 
@@ -23,23 +23,32 @@ $(document).ready(function init(){
     });
 
     
-     console.log(rows[0]);
+    //  console.log(rows[0]);
 
       $('.board').html(rows.join(''))
+          $("[data-col="+5+"]").attr("ondrop", "drop(event)")
+          $("[data-col="+5+"]").attr("ondragover", "allowDrop(event)")
+          console.log($("[data-col="+5+"]"))
 
+// ondrop="drop(event)" ondragover="allowDrop(event)"
 
 });
 
-function getPawn(i, j) {
+function getNum(i, j) {
     var num = (10*i + j)+1;
     if(j===3) {
         $('.num1').html((10*i + j)+1)
+
     }
     if(j===5) {
         $('.num2').html((10*i + j)+1)
     }
         if(num===((10*i + 3)+1)|| num===((10*i + 5)+1)){
-            var res = "";
+        
+          // 
+         
+             var res = '';
+
         }else {
             var res = num;
         
@@ -47,6 +56,23 @@ function getPawn(i, j) {
     
     return res;
 }
+
+
+
+
+        function allowDrop(ev) {
+            ev.preventDefault();
+        }
+
+        function drag(ev) {
+            ev.dataTransfer.setData("text", ev.target.id);
+        }
+
+        function drop(ev) {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("text");
+            ev.target.appendChild(document.getElementById(data));
+        }
 
 // function cellClicked(cell) {
 //     console.log('cell: ', cell);
