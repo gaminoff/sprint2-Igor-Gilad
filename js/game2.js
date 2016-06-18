7083786e98927e7cd3e856fae30bc7f521521ee8
igfeigor <mr.igfe@gmail.com>
2016-06-18 11:29:36 +0300

    f

--- js/game2.js
+++ js/game2.js
@@ -1,117 +1,103 @@
 
 'use strict';
 
-$(document).ready(function init(){
-    // alert('hi');
+var gArrRows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var empty1;
+var empty2;
+var rowShown = 0;
 
-    var rows = [0].map(function(i){
+$(document).ready(function init() {
+    renderBoard();
+    alert('igor');
+    $('tr').hide();
+    $("[data-ro=" + 0 + "]").show();
+    forward();
+    dragAndDrop(empty1, empty2);
+    console.log($("[data-col=" + 5 + "]"));
+});
 
-        var currRowHtml = '<tr>';
-
-        var cells = [0,1,2,3,4,5,6,7,8,9].map(function(j){
-            var cellColor = (j % 2 === 0)? 'dark' : 'bright'; 
-            var num = getNum(i, j);
-
-            return '<td onclick="cellClicked(this)" ' + 
-                    ' data-row="'+i+'" data-col="' + j + '" ' +  
-                    ' class="'+cellColor+' cell'+j+'">' + num  + '</td>';
+function renderBoard() {
+    var rows = gArrRows.map(function (i) {
+        var currRowHtml = '<tr  data-ro="' + i + '">';
+        var cells = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(function (j) {
+            var cellColor = (j % 2 === 0) ? 'dark' : 'bright';
+            var num = getNum(i, j, empty1, empty2);
+            return '<td onclick="cellClicked(this)" ' +
+                ' data-row="' + i + '" data-col="' + j + '" ' +
+                ' class="' + cellColor + ' cell' + j + '">' + num + '</td>';
         });
         currRowHtml += cells.join('');
-
         currRowHtml += '</tr>';
         return currRowHtml;
     });
-
-      $('.board').html(rows.join(''));
-     $('.answer').draggable({revert:true });  // scope:"drop",
-    //  console.log(rows[0]);
-
-   $("[data-col="+5+"]").droppable({ accept : '.answer', drop : function(event, ui) { // scope:"drop"  
-
-            // $("#test").html()
-        //   $("[data-col="+5+"]").appendChild(ul.draggable);
-                //  $(this).append(ui.draggable);
-               $(this).html(ui.draggable.html());
-               ui.draggable.hide();
-}});
-
-     
-          console.log($("[data-col="+5+"]"))
-
-//           function makeDroppable() {
-//     var droppables = document.querySelectorAll('.col-xs-3');
-//     droppables.forEach(function (slot, i) { 
-//         var slotData = $(slot).attr('data-value');  
-//         $(slot).droppable({ drop: handleDrop,
-//                             accept: '.val'+slotData });
-//     });
-// }
-
-// ondrop="drop(event)" ondragover="allowDrop(event)"
-
-});
-
-function getNum(i, j) {
-    var num = (10*i + j)+1;
-    if(j===3) {
-        $('.answer1').html((10*i + j)+1)
-
-    }
-    if(j===5) {
-        $('.answer2').html((10*i + j)+1)
-    }
-        if(num===((10*i + 3)+1)|| num===((10*i + 5)+1)){
-        
-          // 
-         
-             var res = '';
-
-        } else {
-            var res = num;
-        
-    }
-    
-    return res;
+    $('.board').html(rows.join(''));
 }
 
+function forward() {
+    getRandomCells();
+    getEmptySpaces(empty1, empty2);
+    renderAnswers(empty1, empty2);
+    $("[data-ro=" + rowShown + "]").show();
+}
 
+function getNum(i, j) {
+    var num = (10 * i + j) + 1;
+    return num;
+}
 
+function getEmptySpaces(empty1, empty2) {
+    $("[data-col=" + empty1 + "][data-row=" + rowShown + "]").html('');
+    $("[data-col=" + empty2 + "][data-row=" + rowShown + "]").html('');
+}
 
-        // function allowDrop(ev) {
-        //     ev.preventDefault();
-        // }
+function renderAnswers(empty1, empty2) {
 
-        // function drag(ev) {
-        //     ev.dataTransfer.setData("text", ev.target.id);
-        // }
+    $('.answer1').html(empty1 + 1 + 10 * rowShown);
+    $('.answer2').html(empty2 + 1 + 10 * rowShown);
+    $('.answer1').show();
+    $('.answer2').show();
 
-        // function drop(ev) {
-        //     console.log(ev);
-        //     ev.preventDefault();
-        //     var data = ev.dataTransfer.getData("text");
-        //     ev.target.appendChild(document.getElementById(data));
-        
-        // function drop(ev) {
-        //     ev.preventDefault();
-        //     var data = ev.dataTransfer.getData("text");
-        //     ev.target.appendChild(document.getElementById(data));
-        // }
+}
 
+function dragAndDrop(empty1, empty2) {
+    var check = 0;
+    $('.answer').draggable({ scope: rowShown, revert: true });
+    $("[data-col=" + empty1 + "][data-row=" + rowShown + "]").droppable({
+        accept: '.answer1', scope: rowShown, drop: function (event, ui) {
+            $(this).html(ui.draggable.html());
+            ui.draggable.hide();
+            check++;
+            checkEndOfStage(check);
+        }
+    });
+    $("[data-col=" + empty2 + "][data-row=" + rowShown + "]").droppable({
+        accept: '.answer2', scope: rowShown, drop: function (event, ui) {
+            $(this).html(ui.draggable.html());
+            ui.draggable.hide();
+            check++;
+            checkEndOfStage(check);
+        }
+    });
+}
 
-// function cellClicked(cell) {
-//     console.log('cell: ', cell);
-    
+function checkEndOfStage(check) {
+    if (check === 2) {
+        // console.log(gArrRows);
+        rowShown++;
+        if (rowShown < 10) {
+            forward();
+            dragAndDrop(empty1, empty2);
+            console.log(empty1, empty2);
+        } else alert('you did it!!!!!');
+    }
+}
 
-//         var $cell = $(cell);
-//         var data = $cell.data();
-//     if ($cell.html() === KNIGHT) {
-     
-//     } 
-// } 
-// function checkPosibolMovment(date, innerHtml){
-    
-// }
-function handleDropEvent( event, ui ){
-    // ui.draggable.draggable({ revertDuration: 0 });
-     $("[data-col="+5+"]").remove() ;
+function getRandomCells() {
+    empty1 = parseInt(Math.random() * 10);
+    empty2 = parseInt(Math.random() * 10);
+    while (empty1 === empty2) {
+        empty2 = parseInt(Math.random() * 10);
+    }
+    console.log(empty1);
+    console.log(empty2);
 }
